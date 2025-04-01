8fff17626e2ef84421387866f709931db3fe3632
uevitondev <uevitonsantos@outlook.com>
2025-04-01 15:59:44 -0300

    project created

---
+++ src/main/java/com/uevitondev/cashcardsa/CashcardsaApplication.java
@@ -0,0 +1,13 @@
+package com.uevitondev.cashcardsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashcardsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashcardsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uevitondev/cashcardsa/CashcardsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uevitondev.cashcardsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
