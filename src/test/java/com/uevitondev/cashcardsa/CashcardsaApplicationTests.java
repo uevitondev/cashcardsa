fb850c71238a36573b0e6442294407f1d8e3c2f8
uevitondev <uevitonsantos@outlook.com>
2025-04-01 18:21:08 -0300

    TDD tests for rest endpoint cashcards

--- src/test/java/com/uevitondev/cashcardsa/CashcardsaApplicationTests.java
+++ src/test/java/com/uevitondev/cashcardsa/CashcardsaApplicationTests.java
@@ -1,13 +1,48 @@
 package com.uevitondev.cashcardsa;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashcardsaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    void shouldReturnACashCardWhenDataIsSaved() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/100", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id).isNotNull();
+        assertThat(id).isEqualTo(100);
+
+        Double amount = documentContext.read("$.amount");
+        assertThat(amount).isEqualTo(120.00);
+    }
+
+    @Test
+    void shouldNotReturnACashCardWithAndUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/200", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+
+    }
 
 }
