1179246c6b668349017a7fd4415e87219bff21fa
uevitondev <uevitonsantos@outlook.com>
2025-04-02 18:11:57 -0300

    Using TDD to testing list JSON data contract, implement endpoint for get list cashcards, with pagination and sorting

--- src/main/java/com/uevitondev/cashcardsa/cashcard/CashCardController.java
+++ src/main/java/com/uevitondev/cashcardsa/cashcard/CashCardController.java
@@ -1,11 +1,15 @@
 package com.uevitondev.cashcardsa.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +22,17 @@ public class CashCardController {
         this.cashCardRepository = cashCardRepository;
     }
 
+    @GetMapping
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+                ));
+        return ResponseEntity.ok(page.getContent());
+    }
+
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
@@ -27,5 +42,15 @@ public class CashCardController {
             return ResponseEntity.notFound().build();
         }
 
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
     }
 }
--- src/test/java/com/uevitondev/cashcardsa/cashcard/CashCardJsonTest.java
+++ src/test/java/com/uevitondev/cashcardsa/cashcard/CashCardJsonTest.java
@@ -1,9 +1,12 @@
 package com.uevitondev.cashcardsa.cashcard;
 
+import org.assertj.core.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.http.ResponseEntity;
 
 import java.io.IOException;
 
@@ -14,6 +17,19 @@ public class CashCardJsonTest {
 
     @Autowired
     private JacksonTester<CashCard> json;
+
+    @Autowired
+    private JacksonTester<CashCard[]> jsonList;
+
+    private CashCard[] cashCards;
+
+    @BeforeEach
+    void setUp() {
+        cashCards = Arrays.array(
+                new CashCard(100L, 120.00),
+                new CashCard(101L, 1.00),
+                new CashCard(102L, 150.00));
+    }
 
     @Test
     void cashCardSerializationTest() throws IOException {
@@ -30,14 +46,29 @@ public class CashCardJsonTest {
     @Test
     void cashCardDeserializationTest() throws IOException {
         String expected = """
-           {
-               "id":100,
-               "amount":120.00
-           }
-           """;
+                {
+                    "id":100,
+                    "amount":120.00
+                }
+                """;
         assertThat(json.parse(expected))
                 .isEqualTo(new CashCard(100L, 120.00));
         assertThat(json.parseObject(expected).id()).isEqualTo(100);
         assertThat(json.parseObject(expected).amount()).isEqualTo(120.00);
     }
+
+    @Test
+    void cashCardListSerializationTest() throws IOException {
+
+        String expected="""
+         [
+            { "id": 100, "amount": 120.00 },
+            { "id": 101, "amount": 1.00 },
+            { "id": 102, "amount": 150.00 }
+         ]
+         """;
+        assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
+    }
+
 }
